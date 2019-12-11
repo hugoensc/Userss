b05a38a91679c1d37310d33b7c545db7452ba080
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2019-12-11 11:04:12 -0600

    FirstCommit

---
+++ Userss/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Userss.Controllers
+{
+    public class UsersController : Controller
+    {
+        // [HttpPost]
+        // [HttpGet]
+        public IActionResult Index(string data, int age)
+        {
+            //ViewData["id"] = data + " " + age;
+            String datos = data + " " + age;
+            return View("Index", datos);
+        }
+    } 
+}
